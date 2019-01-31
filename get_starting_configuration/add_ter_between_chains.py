edae2b124eab6d968e962a141279e40517fccf52
Tarasovk49 <36845153+Tarasovk49@users.noreply.github.com>
2019-01-31 16:43:23 +0300

    Update add_ter_between_chains.py

--- get_starting_configuration/add_ter_between_chains.py
+++ get_starting_configuration/add_ter_between_chains.py
@@ -9,22 +9,22 @@ for o, a in opts:
         out_filename = a
 
 def add_ter_to_chains(in_filename,out_filename):
-    with open(in_filename) as pdb:
-        with open(out_filename,'w') as write:
+    with open(in_filename) as infile:
+        with open(out_filename,'w') as outfile:
             resid=1
-            for line in pdb.readlines():
+            for line in infile.readlines():
                 if line[:4]=='ATOM':
-                    if int(line[23:26] )<resid:
-                        write.write('TER\n')
-                        write.write(line)
-                        resid = int(line[24:26] )
+                    if int(line[23:26])<resid:
+                        outfile.write('TER\n')
+                        outfile.write(line)
+                        resid = int(line[23:26])
 
-                    elif int(line[23:26] )==resid:
-                        write.write(line)
+                    elif int(line[23:26])==resid:
+                        outfile.write(line)
                     else:
                         resid+=1
-                        write.write(line)
+                        outfile.write(line)
                 else:
-                    write.write(line)
+                    outfile.write(line)
 
 add_ter_to_chains(in_filename,out_filename)
