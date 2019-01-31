601b541ee40643262c2ba6a0068f3e1a7e3b51dd
Tarasovk49 <36845153+Tarasovk49@users.noreply.github.com>
2019-01-31 17:52:50 +0300

    Add files via upload

---
+++ steered_md/gen_index.py
@@ -0,0 +1,55 @@
+# Generates .ndx file with groups needed for pulling: groups POL$N including backbone atoms
+# of polymers and group MEMBRANE including phosphorus atoms of membrane. These groups then
+# used during simulation to compute centers of mass of polymers and membrane.
+# Forces are applied on distances atoms from POL$N groups to pull polymers towards membrane.
+
+import sys
+from getopt import getopt
+
+opts, args = getopt(sys.argv[1:], 'i:o:')
+for o, a in opts:
+    if o == '-i':
+        in_filename = a
+    if o == '-o':
+        out_filename = a
+
+def make_ndx(in_filename, out_filename):
+    with open(in_filename) as pdb:
+        with open(out_filename,'w') as ndx:
+            resid=1000
+            pol_molecule_num = 1 # Counts polymer molecules
+            line_value_num = 0 # Counts written numbers of atoms in strings (numbers are written by 15 in one string)
+            for line in pdb.readlines():
+                if (line[:4]=='ATOM') and ((line[17:21]=='POPC') or (line[17:21]=='DPPC')\
+                                      or (line[17:21]=='DOPC') or (line[17:21]=='CHOL')) \
+                                      and ('P' in line[12:16]):
+                    if int(line[23:26] )<resid:
+                        ndx.write('\n' + '[ MEMBRANE ]' + '\n')
+                        resid = int(line[23:26])
+                        line_value_num = 0
+                    ndx.write(' '+line[6:12])
+                    line_value_num += 1
+                    if line_value_num == 15:
+                        ndx.write('\n')
+                        line_value_num = 0
+                if (line[:4]=='ATOM') and ((line[17:20]=='ST1') or (line[17:20]=='ST2') or (line[17:20]=='MAL')\
+                     or (line[17:20]=='MAR') or (line[17:20]=='MAD') or (line[17:20]=='MA2') or (line[17:20]=='DB1')\
+                     or (line[17:20]=='DB2')) and (('CA' in line[12:16]) or ('CB' in line[12:16])):
+                    if line_value_num == 15:
+                        ndx.write('\n')
+                        line_value_num = 0
+                    if int(line[23:26] )<resid:
+                        ndx.write('\n' + '[ POL' + str(pol_molecule_num) + ' ]' + '\n')
+                        ndx.write(' '+line[6:12])
+                        resid = int(line[23:26])
+                        pol_molecule_num +=1
+                        line_value_num = 0
+                    elif int(line[23:26])==resid:
+                        ndx.write(' '+line[6:12])
+                    else:
+                        resid+=1
+                        ndx.write(' '+line[6:12])
+                    line_value_num += 1
+#    print pol_molecule_num
+    
+make_ndx(in_filename, out_filename)
---
+++ steered_md/gen_mdp.py
@@ -0,0 +1,45 @@
+# Adds pull options to mdp file. Center of mass of each polymer molecule is pulled
+# towards center of mass of membrane by applying harmonic potential until distance become 4 nm
+
+from sys import argv
+from getopt import getopt
+
+opts, args = getopt(argv[1:], 'm:i:o:')
+for o, a in opts:
+    if o == '-m':
+        # input mdp file (everything before pull options)
+        input_mdp = a
+    if o == '-i':
+        # index file with groups POL1, POL2, .., MEMBRANE
+        # POLs will be pulled towards MEMBRANE
+        index = a
+    if o == '-o':
+        # output mdp file with pull options
+        output_mdp = a
+
+def gen_mdp(input_mdp, index, output_mdp):
+    sma_mol_count=0
+    with open(index) as ndx:
+        for line in ndx.readlines():
+            if line[0:5] == '[ POL':
+                sma_mol_count+=1
+    with open(output_mdp,'w') as mdp:
+        with open(input_mdp) as main_mdp:
+            for string in main_mdp.readlines():
+                mdp.write(string)
+            mdp.write('; Pull code'+'\n')
+            mdp.write('pull                    = yes'+'\n')
+            mdp.write('pull_ngroups            = {0}'.format(sma_mol_count+1)+'\n')
+            mdp.write('pull_ncoords            = {0}'.format(sma_mol_count)+'\n')
+            mdp.write('pull_group1_name        = MEMBRANE'+'\n')
+            for i in range(1,1+sma_mol_count):
+                mdp.write('pull_group{0}_name        = POL{1}'.format((i+1),i)+'\n')
+                mdp.write('pull_coord{0}_type        = flat-bottom   ; harmonic biasing force pulling only when distance > pull_coord_init'.format(i)+'\n')
+                mdp.write('pull_coord{0}_geometry    = distance      ; '.format(i)+'\n')
+                mdp.write('pull_coord{0}_groups      = 1 {1}'.format(i,(i+1))+'\n')
+                mdp.write('pull_coord{0}_dim         = Y Y N'.format(i)+'\n')
+                mdp.write('pull_coord{0}_init        = 4             ; distance decreases while it is higher than 4 nm'.format(i)+'\n')
+                mdp.write('pull_coord{0}_k           = 50            ; kJ mol^-1 nm^-2'.format(i)+'\n')
+                mdp.write('\n')
+                                
+gen_mdp(input_mdp, index, output_mdp)
