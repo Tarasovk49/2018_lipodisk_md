b96048dd3451a8931047341535a184aef39ed70c
k-tarasov <36845153+Tarasovk49@users.noreply.github.com>
2019-02-08 13:18:33 +0300

    Update orient_poly.py

--- get_starting_configuration/orient_poly.py
+++ get_starting_configuration/orient_poly.py
@@ -4,13 +4,18 @@ from sys import argv
 from getopt import getopt
 import os
 
-opts, args = getopt(argv[1:], 'm:p:o:n:')
+opts, args = getopt(argv[1:], 'm:p:o:n:',longopts=['r_coeff=','angle_coeff='])
 
 # Defaults
 membrane = 'popc.pdb'
 pol_dir = 'polymer_molecules'
 outfile = 'lipodisk.pdb'
 layers = 2
+# Decrease 2 coefficients below to place polymers with higher density
+# r_coeff = 1.0 is the least value when it is garanteed that none of the polymers overlap
+r_coeff = 0.7
+# angle_coeff = 2.0 is the least value when it is garanteed that none of the polymers overlap
+angle_coeff = 0.9
 
 for o, a in opts:
     if o == '-m':
@@ -21,6 +26,10 @@ for o, a in opts:
         outfile = a
     if o == '-n':
         layers = int(a)
+    if o == '--r_coeff':
+        r_coeff = float(a)
+    if o == '--angle_coeff':
+        angle_coeff = float(a)
         
 # Load membrane
 u_mem = mda.Universe(membrane)
@@ -33,13 +42,6 @@ xmin,ymin,zmin=np.amin(ag_mem.positions[:],0)
 R = np.sqrt((xmax-xmin)**2 + (ymax-ymin)**2)/2
 print('R of membrane is '+str(R))
 
-# Decrease 2 coefficients below to place polymers with higher density
-# r_coeff = 1.0 is the least value when it is garanteed that none of the polymers overlap
-r_coeff = 0.7
-# angle_coeff = 2.0 is the least value when it is garanteed that none of the polymers overlap
-angle_coeff = 0.9
-
-
 # Load polymers, place them in (0,0,0), compute the maximal radius
 pol_list = os.listdir(pol_dir)
 polymers={}
@@ -47,10 +49,13 @@ rmax = 0
 for pol_num, pol_name in enumerate(pol_list):
     u = mda.Universe(pol_dir + '/%s'%pol_name)
     polymers[pol_num] = u.select_atoms("resname DB1 DB2 MAR MAL MA2 MAD ST1 ST2")
+    # Place COM of polymer molecules to (0,0,0)
     polymers[pol_num].translate(-polymers[pol_num].center_of_geometry())
+    # Align first principal axes of polymer molecules along z-axis
     PAX = np.matrix(polymers[pol_num].principal_axes())
     PAXI = PAX.I
     polymers[pol_num].rotate(PAX)
+    # Calculate minmax coordinates of polymer molecules
     xmax, ymax, zmax = np.amax(ag_mem.positions[:],0)
     xmin, ymin, zmin = np.amin(ag_mem.positions[:],0)
     r = np.sqrt((xmax-xmin)**2 + (ymax-ymin)**2)/2
@@ -60,7 +65,6 @@ for pol_num, pol_name in enumerate(pol_list):
 # Place polymers around membrane
 lastvalue = 0
 for b in range(layers):
-#    b = b + 1
     angle = angle_coeff*np.arcsin(rmax/(rmax*(2*b+1)*r_coeff + R))
     k = int(6.28318530718/angle)
     angle = 6.28318530718/k
