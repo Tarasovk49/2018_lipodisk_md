13e06949fd214bbab21581a20a4a0110dd794eee
k-tarasov <36845153+Tarasovk49@users.noreply.github.com>
2019-08-06 13:30:19 +0300

    Update gen_mdp.py

--- steered_md/gen_mdp.py
+++ steered_md/gen_mdp.py
@@ -29,21 +29,23 @@ def gen_mdp(input_mdp, index, output_mdp):
     Adds pull options to mdp file. Center of mass of each polymer molecule is pulled
     towards center of mass of membrane by applying harmonic potential until distance become @R nm
     """
-    sma_mol_count=0
+    polymer_mol_count=0
     with open(index) as ndx:
         for line in ndx.readlines():
             if line[0:5] == '[ POL':
-                sma_mol_count+=1
+                polymer_mol_count+=1
     with open(output_mdp,'w') as mdp:
         with open(input_mdp) as main_mdp:
             for string in main_mdp.readlines():
                 mdp.write(string)
+                
             mdp.write('; Pull code'+'\n')
             mdp.write('pull                    = yes'+'\n')
-            mdp.write('pull_ngroups            = {0}'.format(sma_mol_count+1)+'\n')
-            mdp.write('pull_ncoords            = {0}'.format(sma_mol_count)+'\n')
+            mdp.write('pull_ngroups            = {0}'.format(polymer_mol_count+1)+'\n')
+            mdp.write('pull_ncoords            = {0}'.format(polymer_mol_count)+'\n')
             mdp.write('pull_group1_name        = MEMBRANE'+'\n')
-            for i in range(1,1+sma_mol_count):
+            
+            for i in range(1,1+polymer_mol_count):
                 mdp.write('pull_group{0}_name        = POL{1}'.format((i+1),i)+'\n')
                 mdp.write('pull_coord{0}_type        = flat-bottom   ; harmonic biasing force pulling only when distance > pull_coord_init'.format(i)+'\n')
                 mdp.write('pull_coord{0}_geometry    = distance      ; '.format(i)+'\n')
